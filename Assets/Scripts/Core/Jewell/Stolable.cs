0f8201a047d2891c7496a683e0f4901ee7a605d7
Eduardo Simón Picón <al341513@uji.es>
2018-10-27 15:14:15 +0200

    Hecho Interaccion del player basica

---
+++ Assets/Scripts/Core/Jewell/Stolable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stolable : MonoBehaviour{
+
+    public int moneyValue;
+    public bool enableAlarm;
+    public bool neededToComplete; //strongbox
+
+    public CanvasGroup canvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            canvas.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnItemPickedUp()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
