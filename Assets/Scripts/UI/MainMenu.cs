4f28c41862bbf10ecd252f6a8059dbf82586002f
makastreteam <sergiomacastre@gmail.com>
2018-10-27 04:57:49 +0200

    Menu, HUD, Sonido

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    GameObject other;
+    PauseMenu _audioManager;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void Start()
+    {
+        other = GameObject.FindWithTag("AudioManager");
+        _audioManager = other.GetComponent<PauseMenu>();
+    }
+
+    public void PlayGame()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log ("QUIT");
+		Application.Quit ();
+	}
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("MenuJAMControls");
+    }
+
+    public void Options()
+    {
+        _audioManager.OptionsMainMenu();
+    }
+
+}
