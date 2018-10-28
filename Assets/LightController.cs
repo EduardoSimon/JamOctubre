239a239ec978bd04887588325c8839d9973d9231
oskarter9 <oscar5teruel@gmail.com>
2018-10-28 16:38:50 +0100

    XD

---
+++ Assets/LightController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    public GameObject[] lights;
+    int contador = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<NPC>())
+        {
+            foreach (GameObject light in lights)
+            {
+                light.SetActive(true);
+            }
+
+            contador++;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        contador--;
+
+        if (other.gameObject.GetComponent<NPC>())
+        {
+            if(contador <= 0)
+            {
+                foreach (GameObject light in lights)
+                {
+                    light.SetActive(false);
+                }
+            }
+        }
+    }
+
+
+}
