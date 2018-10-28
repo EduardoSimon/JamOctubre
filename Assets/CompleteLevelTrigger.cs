fea765b82268556acba795da177f7f24587a2fd6
Eduardo Simón Picón <al341513@uji.es>
2018-10-28 15:25:48 +0100

    GOGOGO

---
+++ Assets/CompleteLevelTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompleteLevelTrigger : MonoBehaviour
+{
+
+	public bool debugCompleted = false;
+	private bool playerInside;
+	public Canvas canvas;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<PlayerController>())
+		{
+			playerInside = true;
+		}	
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.GetComponent<PlayerController>())
+		{
+			playerInside = false;
+		}	
+	}
+
+	void Update()
+	{
+		#if UNITY_EDITOR
+		if (debugCompleted)
+		{
+			GameManager.I.canCompleteLevel = true;
+		}
+		#endif
+		
+		if (GameManager.I.canCompleteLevel)
+		{
+			canvas.gameObject.SetActive(true);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			if (GameManager.I.canCompleteLevel)
+			{
+				GameManager.I.levelCompleted = true;
+			}
+		}
+		
+	}
+}
